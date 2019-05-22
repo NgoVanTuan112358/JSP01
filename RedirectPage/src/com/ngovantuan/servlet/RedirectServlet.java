8fd85b6c7639d0a7a1c803e4ba96eebde721f754
= <=>
2019-05-22 19:58:29 +0700

    Bai tap page redirect

---
+++ RedirectPage/src/com/ngovantuan/servlet/RedirectServlet.java
@@ -0,0 +1,61 @@
+package com.ngovantuan.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RedirectServlet
+ */
+@WebServlet("/RedirectServlet")
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RedirectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String pass = request.getParameter("password");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		if(!(name.equals("Ngo Van Tuan") || email.equals("ngovantuan@gmail.com"))) {
+			out.println("Name or email is not correct");
+		}else if(!pass.equals("123456789")) {
+			out.println("Password is not correct");
+		}else {
+			request.setAttribute("name", name);
+			request.setAttribute("password", pass);
+			 
+			RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/ShowInfoLogIn");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ RedirectPage/src/com/ngovantuan/servlet/ShowInfoLogIn.java
@@ -0,0 +1,50 @@
+package com.ngovantuan.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowInfoLogIn
+ */
+@WebServlet("/ShowInfoLogIn")
+public class ShowInfoLogIn extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowInfoLogIn() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		String name = (String)request.getAttribute("name");
+		String pass = (String)request.getAttribute("password");
+		
+		out.println("Welcome : "+name);
+		out.println("Your password : "+pass);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
